123c67fd39829846c8b4e6fe4211e2a0554a4df4
Brandon Carson <beecars@pm.me>
2023-09-11 20:50:46 -0500

    revert adhoc change from last year

--- getSUV_byClass.m
+++ getSUV_byClass.m
@@ -1,9 +1,9 @@
-function pct_gtr_14_arr = getSUV_byClass(pd_idx)
+function SUV_struct = getSUV_byClass(pd_idx)
 %GETTOTALSUV_BYCLASS extracts total SUV from associated mask and
 %FLT-PET data by downsampling the mask data in the axial plane to
-%match the FLT-PET data and aggregating the masked FLT-PET SUV values. 
+%match the FLT-PET data and aggregating the masked FLT-PET SUV values.
 %Programmed to extract for the "spine", "pelvi", and "stern" volume
-%mask variables, but can be edited for other object class names. 
+%mask variables, but can be edited for other object class names.
 
 tSUV_spine = SUV_wrapper(pd_idx, 'spine');
 tSUV_pelvi = SUV_wrapper(pd_idx, 'pelvi');
@@ -11,9 +11,10 @@ tSUV_stern = SUV_wrapper(pd_idx, 'stern');
 
 pct_gtr_14_arr = [tSUV_spine, tSUV_pelvi, tSUV_stern];
 
-% SUV_struct.pd_idx = pd_idx;
-% SUV_struct.tSUV_spine = tSUV_spine;
-% SUV_struct.tSUV_pelvi = tSUV_pelvi;
-% SUV_struct.tSUV_stern = tSUV_stern;
+SUV_struct.pd_idx = pd_idx;
+SUV_struct.pct_gtr_14 = pct_gtr_14_arr
+SUV_struct.tSUV_spine = tSUV_spine;
+SUV_struct.tSUV_pelvi = tSUV_pelvi;
+SUV_struct.tSUV_stern = tSUV_stern;
 
 end
